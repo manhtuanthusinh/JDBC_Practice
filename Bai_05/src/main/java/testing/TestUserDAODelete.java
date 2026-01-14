2250df3b4cfdc5bce627fc17a5a8a41b5ec6df0d
Tuan Lai <tuanlaimanh20041@gmail.com>
2026-01-14 21:28:28 +0700

    fix: small configs to match with mysql docker

--- Bai_05/src/main/java/testing/TestUserDAODelete.java
+++ Bai_05/src/main/java/testing/TestUserDAODelete.java
@@ -6,8 +6,8 @@ import model.User;
 public class TestUserDAODelete {
     public static void main(String[] args) {
 
-        for (int i = 4; i < 10000; i++) {
-            User user = new User(i, "abc", "xyz", "333");
+        for (int i = 0; i < 10000; i++) {
+            User user = new User(i, "Testing_" + i, "", "");
             UserDAO.getInstance().delete(user);
         }
 
--- Bai_08/src/main/java/testing/TestDeleteUserDAO.java
+++ Bai_08/src/main/java/testing/TestDeleteUserDAO.java
@@ -7,7 +7,7 @@ public class TestDeleteUserDAO {
     public static void main(String[] args) {
         System.out.println("============ TEST DELETE ============");
         User newUser = new User();
-        newUser.setId(1);
+        newUser.setId(10000);
         UserDAO.getInstance().delete(newUser);
     }
 }
